442047a153c45da9913447c952c26d17027370fc
Charles Fettinger <charles@Oncorporation.com>
2023-08-15 11:49:14 -0700

    Update yt_get_json
    Add youtube API Get and filter with text
    
    Update get_yt_json.py
    
    fix minor bug
    
    Add basic youtube API filter
    
    based up on youtube_channel_transcript_api

---
+++ yt_get_json.py
@@ -0,0 +1,31 @@
+try:
+    import youtube_channel_transcript_api as ycta
+except ImportError:
+    print(f"Error, Module required! try: pip install youtube_channel_transcript_api")
+import json
+
+def main():    
+    channel = input("enter youtube channel id  (@mychannel): ")
+    yt_API = input("enter youtube API key: ")
+    channel_get = ycta.YoutubeChannelTranscripts(channel, yt_API)
+
+    nf = channel_get.get_transcripts()
+    nf_data, nf_errors = nf
+    len_nf_data = len(nf_data)
+    print(f'Fenton Raw:{len_nf_data}')
+    ###f = open('nf.txt','w')
+
+    filter_string = input("Enter filter text: ")
+    # Filter nodes that contain the filter_string
+    filtered_data = {
+        key: value
+        for key, value in nf_data.items()
+        if any(filter_string  in caption["text"] for caption in value["captions"])
+    }
+    len_nf_f_data = len(filtered_data)
+    with open(f'{channel}_filtered.json', 'w') as fp:
+        json.dump(filtered_data, fp, indent=4)
+    print(f'Fenton Filtered:{len_nf_f_data}')
+
+if __name__ == "__main__":
+    main()
